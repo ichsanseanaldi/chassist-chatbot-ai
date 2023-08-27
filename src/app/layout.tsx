1ce90acc24f2adcc2dfe280321794ea78685e26b
ichsan <ichsanseanaldi10@gmail.com>
2023-08-27 16:53:30 +0700

    meta & style update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,15 +11,18 @@ const nunito = Nunito({
 
 export const metadata: Metadata = {
   title: 'Chassist | Chatbot AI',
-  description: "Chassist is a chatting bot app which uses ChatGPT AI from OpenAI, it's free to use and doesn't require Login.",
+  description: "Chassist is a chatting bot app which uses ChatGPT AI from OpenAI, it's free to use and doesn't require Login. Hosted in Vercel",
   generator:"Next.js",
   publisher:"Vercel",
   applicationName:"Chassist | Chatbot AI",
   robots:"index, follow",
-  keywords:["Chatbot","AI","Chassist","ChatGPT"],
+  keywords:["Chatbot","AI","Chassist","ChatGPT","Vercel","OpenAI"],
   referrer:"origin",
   creator:"Ichsan Seanaldi Permana",
   alternates:{canonical:"https://chassist.vercel.app/"},
+  verification:{
+    google:"VPJMCbyK5vNMPtSxKkB6x261bchSwi7bZ-B4VvkgM08"
+  }
 }
 
 export default function RootLayout({
